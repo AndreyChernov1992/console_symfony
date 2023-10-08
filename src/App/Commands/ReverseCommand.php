90a7c529af665202ed74e8517a2545b562597aae
Coldsun <robotsdontcry92@gmail.com>
2023-10-08 18:36:14 +0700

    psr

--- src/App/Commands/ReverseCommand.php
+++ src/App/Commands/ReverseCommand.php
@@ -8,22 +8,25 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ReverseCommand extends Command {
+class ReverseCommand extends Command 
+{
 
-    protected function configure() {
+    protected function configure() 
+    {
         $this->setName("reverse")
             ->setDescription("Your string reverse!")
             ->setHelp("First custom command test")
             ->addArgument("file", InputArgument::REQUIRED, "Put file or write your string");
     }
 
-    protected function execute(InputInterface  $input, OutputInterface $output) {
-
+    protected function execute(InputInterface  $input, OutputInterface $output) 
+    {
         $reverse = new Reverse;
         $file = $input->getArgument("file");
         $string = file_exists(ROOT . "$file") ? file_get_contents(ROOT . $file) : $file;
         $output->writeln("Reversing $string");       
         $output->writeln($reverse->reverse($string));
+        
         return 0;
     }
 }
